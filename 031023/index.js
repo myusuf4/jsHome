293970ff5593bea32b4d7c6413e74351f3311a77
Myusuf04 <nurmurzayevmuhammad@gmail.com>
2023-03-10 11:19:31 +0500

    Numbers

---
+++ 031023/index.js
@@ -0,0 +1,49 @@
+// let num=10;
+// let bin=num.toString(2);
+
+// console.log(bin);
+// let noll=0;
+// let bir=0;
+
+// for(i=0;i<bin.length;i++){
+//     bin[i]==1?bir++:noll++;
+// }
+// console.log(`bir soni ${bir} marta qatnashgan`);
+// console.log(`nol soni ${noll} marta qatnashgan`);
+
+
+// ======
+// let a=1,b=2,c=7,d=10;
+// console.log(Math.min(a,b,c,d));
+// console.log(Math.max(a,b,c,d));
+
+
+// ========
+
+// floor=> faqat pastga songa intilgan xolda yahlitlaydi;
+// let num=2.8;
+// console.log(Math.floor(num));
+// let n=-2.9;
+// console.log(Math.floor(n));
+
+// ceil => faqat yuqori songa qarab yaxlitlaydi;
+
+// console.log(Math.ceil(num));
+// console.log(Math.ceil(n));
+
+// trunc=> faqat butun qismini olib kasr qismini ignore qiladi
+// console.log(Math.trunc(num));
+// console.log(Math.trunc(n));
+
+
+// parseInt=> faqat butun qismini oladi
+
+
+
+// ==========
+// function randomInteger(min, max) {
+//     let rand = min + Math.random() * (max - min);
+//     return Math.round(rand);
+//   }
+  
+//   alert( randomInteger(1, 50));
