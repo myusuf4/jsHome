7f6b976bf0d89a7e2c5fbd81dbc97403bb4e88ae
myusuf4 <nurmurzayevmuhammad@gmail.com>
2023-05-05 22:58:15 +0500

    CRUD SortByYear

---
+++ 050523/index.js
@@ -0,0 +1,17 @@
+// let user=[
+
+//     {id:1,name:"Muhammad",year:2007},
+//     {id:2,name:"Abdulloh",year:2003},
+//     {id:3,name:"Abdurahmon",year:2000},
+//     {id:4,name:"Abdulaziz",year:2001},
+//     {id:5,name:"Abdujalol",year:1999},
+//     {id:6,name:"AbduKarim",year:1995},
+//     {id:7,name:"Yusuf",year:2004},
+    
+// ];
+
+// const sortByYear =function(user) {
+//     let res=user.sort((a,b)=>a.year-b.year)
+//     return res;
+// }
+// console.log(sortByYear(user));
