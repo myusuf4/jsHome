3c026046980c5b519f4c98df66d5a4a12517727d
myusuf4 <nurmurzayevmuhammad@gmail.com>
2023-05-08 22:59:02 +0500

    Object revise

--- 050523/index.js
+++ 050523/index.js
@@ -15,3 +15,20 @@
 //     return res;
 // }
 // console.log(sortByYear(user));
+
+
+let menu = {
+    width: 200,
+    height: 300,
+    title: "My menu"
+  };
+
+  function multiplyNumeric(obj) {
+    for (let key in obj) {
+      if (typeof obj[key] == 'number') {
+        obj[key] *= 2;
+      }
+    }
+  }
+  multiplyNumeric(menu);
+  console.log(menu);
